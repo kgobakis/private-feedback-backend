ba25d94f20ecf622a38d42b93dd970d291437f8d
harry <hawoonjung98@gmail.com>
2019-09-11 18:47:39 -0400

    noop

--- src/main/java/com/example/kostas/demoeeee/DemoeeeeApplication.java
+++ src/main/java/com/example/kostas/demoeeee/DemoeeeeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoeeeeApplication {
 
 	public static void main(String[] args) {
+		System.out.println("");
 		SpringApplication.run(DemoeeeeApplication.class, args);
 	}
 
